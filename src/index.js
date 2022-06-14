d5a1be5a969f84a206ec5d43f76e7a2c852716bd
KateBlazhko <kateugnicheva@gmail.com>
2022-06-14 23:33:57 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-//const bracketsConfig = [['(', ')'], ['|', '|']];
-//const str = '|()|'
 
 module.exports = function check(str, bracketsConfig) {
-  let arrOpen = [];
-  let arrClose = [];
-  let arrFull = str.split('');
-  let checkOpen;
-  let checkClose;
-  let a;
-  do {
-    a = 0;
-    bracketsConfig.forEach(coupleOfbracket => {
-      checkClose = arrFull.indexOf(coupleOfbracket[1]);
+  const stack = [];
+  
+  for (const bracket of str) {
+    
+    const coupleOfbracket = bracketsConfig.find(coupleOfbracket => {
+        if (coupleOfbracket[0] === bracket) return true
+        if (coupleOfbracket[1] === bracket) return true
+      return false
+    })
 
-      if (coupleOfbracket[0] === coupleOfbracket[1]) {
-        checkClose = arrFull.indexOf(coupleOfbracket[1], (checkClose + 1));
-      };
+    if (coupleOfbracket[0] === coupleOfbracket[1]) {
 
-      checkOpen = arrFull.lastIndexOf(coupleOfbracket[0], checkClose - 1);
+      if (stack[stack.length - 1] === bracket) stack.pop()
+      else stack.push(bracket)
 
-      if (arrFull[checkClose - 1] === coupleOfbracket[0]) {
-        arrFull.splice(checkClose, 1);
-        arrFull.splice(checkOpen, 1);
-        a = 1;
+    } else {
+
+      if (coupleOfbracket[0] === bracket) {
+        stack.push(bracket)
+
+      } else {
+        const lastStackBracket = stack.pop()
+
+        if (lastStackBracket !== coupleOfbracket[0]) {
+          return false
+        }
+
       }
-    });
+    }
 
-  } while (a && (arrFull.length !== 0))
+  }
 
-  return (arrFull.length === 0) ? true : false;
-
-
+  return stack.length === 0
 }
 
-//console.log( check(str, bracketsConfig) );
+// console.log( check(str, bracketsConfig) );
 // node ./src/index.js brackets
